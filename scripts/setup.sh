efed66a7dd10d253c2bf45e46a0b1a641c8a2a7a
Ashish Gupta <ashishmgupta@hotmail.com>
2023-08-15 12:00:08 -0400

    changed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,18 +12,17 @@ sudo apt-get -y install gzip
 echo "crearing a directory named ddos_tests"
 sudo mkdir ddos_tests
 echo "switching to ddos_tests directory"
-cd ddos_tests
-if [ -e /usr/share/wordlists/rockyou.txt.gz ]; then
-    echo "/usr/share/wordlists/rockyou.txt.gz downloaded. Extracting to rockyou.txt in current directory"
-    sudo gzip -d  /usr/share/wordlists/rockyou.txt.gz .
+cd ~/ddos_tests
+if [ -e /usr/share/wordlists/rockyou.txt]; then
+    echo "/usr/share/wordlists/rockyou.txt was downloaded from the package wordlist"
 else
-    echo "/usr/share/wordlists/rockyou.txt.gz does not exist."
+    echo "/usr/share/wordlists/rockyou.txt does not exist."
 fi
 echo "downloading the scripts from github"
 wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/create_random_domain_queries.sh -O create_random_domain_queries.sh
 wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/dnsperf_test.sh -O dnsperf_test.sh
 echo "Allow execute for the script"
-sudo chmod +x create_random_domain_queries.sh 
-sudo chmod +x dnsperf_test.sh
+sudo chmod +x ~/ddos_tests/create_random_domain_queries.sh 
+sudo chmod +x ~/ddos_tests/dnsperf_test.sh
 echo "preparing a file with DNS queries for random domain names"
-sudo ./create_random_domain_queries.sh /usr/share/wordlists/rockyou.txt 
+sudo ~/ddos_tests/create_random_domain_queries.sh /usr/share/wordlists/rockyou.txt 
