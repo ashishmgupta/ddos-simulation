ae2c0fd6185be30b43eafd4cf9dd077c9883b9e2
Ashish Gupta <ashishmgupta@hotmail.com>
2023-08-15 10:42:41 -0400

    Changed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,11 @@ echo "crearing a directory named ddos_tests"
 sudo mkdir ddos_tests
 echo "switching to ddos_tests directory"
 sudo cd ddos_tests
-echo "extracting the rockyou.txt from /usr/share/wordlists/rockyou.txt.gz to teh current directory"
+echo "extracting the rockyou.txt from /usr/share/wordlists/rockyou.txt.gz to the current directory"
 gzip -d  /usr/share/wordlists/rockyou.txt.gz .
 echo "downloading the scripts from github"
+wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/create_random_domain_queries.sh
+echo "Allow execute for the script"
+sudo chmod +x create_random_domain_queries.sh 
+echo "preparing a file with DNS queries for random domain names"
+sudo ./create_random_domain_queries.sh /usr/share/wordlists/rockyou.txt 
