360406229dfe157066d8e853cd2d55e9de93176f
Ashish Gupta <ashishmgupta@hotmail.com>
2023-08-15 12:03:55 -0400

    corrected

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,14 +13,14 @@ echo "crearing a directory named ddos_tests"
 sudo mkdir ddos_tests
 echo "switching to ddos_tests directory"
 cd ~/ddos_tests
-if [ -e /usr/share/wordlists/rockyou.txt]; then
+if [ -e /usr/share/wordlists/rockyou.txt ]; then
     echo "/usr/share/wordlists/rockyou.txt was downloaded from the package wordlist"
 else
     echo "/usr/share/wordlists/rockyou.txt does not exist."
 fi
 echo "downloading the scripts from github"
-wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/create_random_domain_queries.sh -O create_random_domain_queries.sh
-wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/dnsperf_test.sh -O dnsperf_test.sh
+sudo wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/create_random_domain_queries.sh -O ~/ddos-tests/create_random_domain_queries.sh
+sudo wget https://raw.githubusercontent.com/ashishmgupta/ddos-simulation/main/scripts/dnsperf_test.sh -O ~/ddos-tests/dnsperf_test.sh
 echo "Allow execute for the script"
 sudo chmod +x ~/ddos_tests/create_random_domain_queries.sh 
 sudo chmod +x ~/ddos_tests/dnsperf_test.sh
